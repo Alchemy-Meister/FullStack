c2347b7377e5a5b458b1953ebc3750d4b6875d9d
Alchemy-Meister <jesusesma@gmail.com>
2014-11-30 13:45:45 +0100

    Unnecessary code has been removed

--- client/app/game/mygames/myGames.controller.js
+++ client/app/game/mygames/myGames.controller.js
@@ -3,24 +3,6 @@
 angular.module('fullStackApp')
   .controller('MyGamesCtrl', function ($scope, $http, $stateParams, $location, $q, Auth, User, socket, Store) {
 
-    if($stateParams.region && $stateParams.language && $stateParams.version
-        && $stateParams.term && $stateParams.bucket && $stateParams.game_content_type
-        && $stateParams.sort && $stateParams.direction && $stateParams.size
-        && $stateParams.start) {
-            $scope.newThing = $stateParams.term;
-            searchGame({
-            "region": $stateParams.region,
-            "language": $stateParams.language,
-            "version": $stateParams.version,
-            "term": $stateParams.term, 
-            "bucket": [$stateParams.bucket],
-            "game_content_type": [$stateParams.game_content_type],
-            "sort": $stateParams.sort,
-            "direction": $stateParams.direction,
-            "size": $stateParams.size,
-            "start": $stateParams.start
-        });
-    }
     var user = User.get(function() {
         if(user.games.length > 0) {
             User.getGames({id: user._id},function (games) {
@@ -51,44 +33,6 @@ angular.module('fullStackApp')
             $scope.searchError = 'No games at the moment.'
         }
     });
-
-   	$scope.searchThing = function() {
-    	if($scope.newThing === '') {
-            return;
-    	}
-    	$location.search({
-            "region": "ES",
-            "language": "es",
-            "version": "999",
-            "term": $scope.newThing, 
-            "bucket": ["games"],
-            "game_content_type": ["games"],
-            "sort": "name",
-            "direction": "asc",
-            "size": "9999",
-            "start": "0"
-        });
-    };
-
-    function searchGame(json) {
-        Store.search(json, function (data) {
-            if(data.links.length > 0) {
-                angular.forEach(data.links, function (link) {
-                    if(link.default_sku && link.default_sku.entitlements &&
-                            link.default_sku.entitlements[0] && link.default_sku.entitlements[0].drms &&
-                                link.default_sku.entitlements[0].drms[0]) {
-                                    displaySize(link.default_sku.entitlements[0].drms[0], function (err) {
-                                        $scope.searchError = '';
-                                        $scope.search_results = data.links;
-                                    });
-                    }
-                });
-            } else { 
-                $scope.search_results = [];
-                $scope.searchError = 'No data was found.';
-            }
-        });
-    };
 
     function displaySize(json, cb) {
     	var fileSizeUnit = {
